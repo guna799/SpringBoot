9af9e8f1d7f92a6dc2ee0873428f62ca5897d924
guna799 <120400515+guna799@users.noreply.github.com>
2025-05-09 09:50:48 +0530

    Update LogInService.java

--- SpringBoot/SpringBoot/src/main/java/com/springBoot/services/LogInService.java
+++ SpringBoot/SpringBoot/src/main/java/com/springBoot/services/LogInService.java
@@ -1,55 +1,67 @@
 package com.springBoot.services;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.springBoot.model.Employ;
 import com.springBoot.model.LogIn;
 import com.springBoot.repository.LoginRepo;
 
 @Service
 public class LogInService {
 
-	private static final Logger LOG = Logger.getLogger(LogInService.class.getName());
+    private static final Logger LOG = Logger.getLogger(LogInService.class.getName());
 
-	@Autowired
-	LoginRepo loginRepo;
+    @Autowired
+    LoginRepo loginRepo;
 
-	public boolean login(String user, String password) {
-		// TODO Auto-generated method stub
-		LOG.info("User Name -------------> " + user);
-		LOG.info("Password  -------------> " + password);
-		//List<LogIn> logInList=loginRepo.findByUserNameAndPassword(user, password);
-		int count = loginRepo.logIn(user, password);
-		LOG.info("count  ----------------> " + count);
-		if(count==1) {
-			return true;
-		}
+    public boolean login(String user, String password) {
+        try {
+            // Log the login attempt details
+            LOG.info("User Name -------------> " + user);
+            LOG.info("Password  -------------> " + password);
+            
+            // Check login credentials
+            int count = loginRepo.logIn(user, password);
+            LOG.info("count  ----------------> " + count);
+            
+            // Return true if credentials are valid, else false
+            if (count == 1) {
+                return true;
+            }
+        } catch (Exception e) {
+            // Log the exception if something goes wrong during the login attempt
+            LOG.error("Error occurred while attempting to log in with user: " + user, e);
+        }
+        return false;
+    }
 
-		return false;
-	}
+    public LogIn loginDetails(String userName) {
+        try {
+            // Retrieve login details by userName
+            LogIn login = loginRepo.findByUserName(userName);
+            
+            // Return login details
+            return login;
+        } catch (Exception e) {
+            // Log the exception if something goes wrong while retrieving login details
+            LOG.error("Error occurred while retrieving login details for user: " + userName, e);
+        }
+        return null;
+    }
 
-	public LogIn loginDetails(String userName) {
-		// TODO Auto-generated method stub
-		
-//		LogIn login=loginRepo.LoginDetails(userName);
-		
-		LogIn login=loginRepo.getById(userName);
-		return login;
-	}
-
-	public List<LogIn> getAllLogin() {
-		// TODO Auto-generated method stub
-		List<LogIn> logInList = new ArrayList<>();
-		logInList=loginRepo.findAll();
-		return logInList;
-	};
-
+    public List<LogIn> getAllLogin() {
+        List<LogIn> logInList = new ArrayList<>();
+        try {
+            // Retrieve all login records
+            logInList = loginRepo.findAll();
+        } catch (Exception e) {
+            // Log the exception if something goes wrong while fetching all login records
+            LOG.error("Error occurred while fetching all login records.", e);
+        }
+        return logInList;
+    }
 }
